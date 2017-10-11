d4bcad283f519e2b2d8b599510bccf05b45146af
Romain Poirot <romain.poirot@supelec.fr>
2017-10-11 10:27:40 +0200

    Petit test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from xor.neuronLayer import *
+from xor.network import *
 
 
 def f(x):
@@ -9,7 +10,13 @@ def f(x):
 
 
 fn = np.vectorize(f)
+
+
 nl = NeuronLayer(2, 1, fn)
-nl.weights = np.array([[1],[0]])
-print(nl.compute(np.array([5,5])))
+nl.weights = np.array([[1], [0]])
+print(nl.compute(np.array([5, 5])))
 print(nl.activation_levels)
+
+
+net = Network(np.array([2, 2, 1]), np.array([fn, fn]))
+print(nl.compute(np.array([-5, 5])))
