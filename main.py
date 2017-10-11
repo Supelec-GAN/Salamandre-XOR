3271cb267bb0ceae6631b51df4ebca94b863bcbe
PWitz <paul77witz@gmail.com>
2017-10-11 23:52:10 +0200

    Merge branch 'dev' of C:\Users\Paul\PycharmProjects\Salamandre-XOR with conflicts.

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from xor.neuronLayer import NeuronLayer
 from xor.network import Network
 from function.sigmoid import Sigmoid
 
 
-def f(x):
-    if x >= 0:
-        return 1
-    return -1
+pt1 = np.array([-1, -1])
+pt2 = np.array([-1, 1])
+pt3 = np.array([1, 1])
+pt4 = np.array([1, -1])
+pt = [pt1, pt2, pt3, pt4]
+
+activation_functions = np.array([Sigmoid(1)])
+neurons_count = np.array([2, 1])
+net = Network(neurons_count, activation_functions)
 
 
-fn = np.vectorize(f)
 
-g = Sigmoid(1)
+test = False
+while not test:
+    test = True
+    order = np.random.permutation(4)
+    for i in range(4):
+        output = net.compute(pt[order[i]], activation_functions)
 
-nl = NeuronLayer(g, 2, 1)
-nl.weights = np.array([[1], [0]])
-print(nl.compute(np.array([5, 5])))
-print(nl.activation_levels)
+        if order[i] == 0:
+            reference = -1
+        else:
+            reference = 1
 
+        if net.error(output, reference) > 0.25:
+            test = False
+        net.backprop(0.001, 0.2, pt[order[i]], reference)
 
-net = Network(np.array([2, 2, 1]), np.array([g, g]))
-print(nl.compute(np.array([-5, 5])))
