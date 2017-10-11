54b9eabcbfe6094e4fac3f02f427c070ffdd94a2
F. Bouvier d'Yvoire <722_francois@live.fr>
2017-10-11 21:36:30 +0200

    Backprop au niveau des layers

--- xor/neuronLayer.py
+++ xor/neuronLayer.py
@@ -28,3 +28,28 @@ class NeuronLayer:
         self.activation_levels = np.dot(inputs, self._weights) - self._bias
         self.output = self._activation_function(self.activation_levels)
         return self.output
+
+    def derivate(self, delta):
+        return (self.activation_function(self.output) - self.activation_function(self.output + delta))/delta
+
+    def backprop(self, out_influence, eta, delta, input_layer, last=False):
+        if last:
+            out_influence = self.derivate(delta)
+        weight_influence = self.calculate_weight_influence(input_layer, out_influence)
+        self.update_weights(eta, weight_influence)
+        in_influence = self.derivate_error(out_influence)
+        return in_influence
+
+    def update_weights(self, eta, weight_influence):
+        self.weights(self.weigths + eta*weight_influence)
+
+    def calculate_weight_influence(self, input_layer, out_influence):
+        S = self.activation_levels
+        g_prime = self._activation_function.derivate(S)
+        return np.dot(np.dot(input_layer, g_prime), out_influence)
+
+    def derivate_error(self, out_influence):
+        WT = np.transpose(self._weights)
+        S = self.activation_levels
+        g_prime = self._activation_function.derivate(S)
+        return np.dot(np.dot(WT, g_prime), out_influence)
