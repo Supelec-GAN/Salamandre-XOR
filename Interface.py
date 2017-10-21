74bdd91a34951f03806c1cde0d4cd4d4535ae6e1
Delmassad <matthieu.delmas@supelec.fr>
2017-10-21 17:49:37 +0200

    !!! Absolument pas terminé !!! La fonction learning manager est prête, trace l'erreur moyenne sur les n appretissages, et l'erreur moyenne (prise dans un batch_test) toutes les n itérations au sein d'un apprentissage Reste à la classifier, et à mettre dans le main l'appel de cette fonction

--- Interface.py
+++ Interface.py
@@ -28,27 +28,47 @@ def print_grid_net(net, grid):
     plt.show()
 
 
-def learning_manager (batch, parrallel_learnings, activation_functions, neurons_count, iterations, eta) :
+def learning_manager (batch, batch_test, parrallel_learnings, activation_functions, neurons_count, eta, test_period) :
+    iterations = len(batch)
+    iterations_test = len(batch_test)
     iterations_left = iterations
-    errors = np.zeros((iterations, parrallel_learnings))
-    output_list = np.zeros((iterations, parrallel_learnings))
-    reference_list = np.zeros((iterations, parrallel_learnings))
+    errors_during_learning = np.zeros((iterations, parrallel_learnings))
+    errors_during_test = np.zeros((iterations_test, parrallel_learnings))
+    mean_error_during_test = np.rezos((iterations//test_period, parrallel_learnings))
+    output_list_learning = np.zeros((iterations, parrallel_learnings))
+    output_list_test = np.zeros((iterations_test, parrallel_learnings))
+    reference_list_learning = np.zeros((iterations, parrallel_learnings))
+    reference_list_test = np.zeros((iterations_test, parrallel_learnings))
 
     for i in range(parrallel_learnings):
         net = Network(neurons_count, activation_functions)
 
-        while iterations_left > 0 and errors[i][-1] > 0.01 :
+        while iterations_left > 0 and errors_during_learning[i][-1] > 0.01 :
             output = net.compute(batch[iterations-iterations_left])
-            output_list[iterations-iterations_left][i] = output
+            output_list_learning[iterations-iterations_left][i] = output
             reference = fonction_test(batch[iterations-iterations_left])
-            reference_list[iterations-iterations_left][i] = reference
-            errors[iterations-iterations_left][i] = net.error(output,reference)
+            reference_list_learning[iterations-iterations_left][i] = reference
+            errors_during_learning[iterations-iterations_left][i] = net.error(output,reference)
             net.backprop(eta, batch[iterations-iterations_left], reference)
             iterations_left -= 1
 
+            if iterations_left % test_period == 0 :
+                for k in range(len(batch_test)) :
+                    output = net.compute(batch_test[k])
+                    output_list_test[k][i] = output
+                    reference = fonction_test(batch_test[k])
+                    reference_list_test[k][i] = reference
+                    errors_during_test[k][i] = net.error(output,reference)
+                mean_error_during_test[iterations_left//test_period][i] = np.mean(errors_during_test, axis = 0)
 
-    mean_error = np.mean(errors, axis = 1)
-    plt.plot(range(iterations), mean_error)
-    plt.xlabel('Itérations')
-    plt.ylabel('Erreur moyenne sur'+parrallel_learnings + "apprentissages")
-    plt.title ("Evolution de l'erreur au fur et a mesure des apprentissages")
+
+plt.plot(range(len(batch)//test_period),[mean_error_during_test[k][-1] for k in range(len(mean_error_during_test))])
+plt.ylabel("Erreur moyenne sur le batch de test" )
+plt.xlabel ("Occurences des tests")
+plt.title("Evolution de l'erreur effectuée toutes les"+test_period+"itérations d'apprentissage")
+
+mean_error = np.mean(errors_during_learning, axis = 1)
+plt.plot(range(iterations), mean_error)
+plt.xlabel('Itérations')
+plt.ylabel('Erreur moyenne sur'+parrallel_learnings + "apprentissages")
+plt.title ("Evolution de l'erreur au fur et a mesure des apprentissages")
